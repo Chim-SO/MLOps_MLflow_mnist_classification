23fd5f380f45e3510604d3ab6f9387f85b63591b
Chim-SO <chimso1994@gmail.com>
2023-02-22 13:20:23 +0100

    Fix bug.

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def load_data(path):
     df = pd.read_csv(path)
-    x = df.drop('label', axis=1)
-    y = df['label']
+    x = df.drop(columns=[df.columns[-1]])
+    y = df.iloc[:, -1]
     return x.to_numpy(), y.to_numpy()
